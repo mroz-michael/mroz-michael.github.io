024ed15fdc4673f43826f53cf540cbe3f0e35c62
Michael Mroz <michaelmroz01111@gmail.com>
2024-12-19 18:24:33 -0400

    switched from BrowserRouter to HashRouter for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './styles/App.css'
 import Home from './components/Home';
 import Projects from './components/Projects'
 import Experience from './components/Experience';
-import { BrowserRouter as Router,  Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router,  Route, Routes, Link } from 'react-router-dom';
 function App() {
   
   
